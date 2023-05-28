59fe353289aaca8c06c7a0d6670025945debbbd7
IZevsl <michael.lyvak@gmail.com>
2023-05-28 22:04:20 +0300

    added permissions, telegram_bot, approve_place feature

--- geo/models.py
+++ geo/models.py
@@ -1,10 +1,14 @@
 from django.contrib.gis.db import models
 
+from user.models import User
+
 
 class Place(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    geom = models.PointField(srid=4326, null=True, blank=True)
+    description = models.TextField(blank=True)
+    geom = models.PointField(srid=4326, unique=True)
+    aproved = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.name
--- geo/views.py
+++ geo/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from rest_framework import generics, status, viewsets
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import fromstr
 
-# Create your views here.
+from geo.models import Place
+from geo.serializers import (
+    PlaceSerializer,
+    PlaceListSerializer
+)
+
+
+class PlaceListPagination(PageNumberPagination):
+    page_size = 3
+    max_page_size = 3
+
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+    pagination_class = PlaceListPagination
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    @action(
+        detail=True,
+        methods=["get"],
+        url_path="approve",
+        permission_classes=(IsAdminUser,),
+    )
+    def approve_place(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.aproved:
+            return Response(
+                {"detail": "Cannot update an already approved place."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        instance.aproved = True
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def get_queryset(self):
+        queryset = Place.objects.all()
+
+        name = self.request.query_params.get("name")
+
+        if name:
+            queryset = Place.objects.filter(name__icontains=name)
+
+        if self.request.user.is_superuser:
+            return queryset
+
+        return queryset.filter(aproved=True)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlaceListSerializer
+
+        return PlaceSerializer
+
+
+class NearestPlaceView(generics.RetrieveAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+
+    def get_object(self):
+        latitude = self.request.query_params.get("latitude", None)
+        longitude = self.request.query_params.get("longitude", None)
+
+        if latitude is not None and longitude is not None:
+            pnt = fromstr(f"POINT({latitude} {longitude})", srid=4326)
+            return (
+                self.get_queryset()
+                .annotate(distance=Distance("geom", pnt))
+                .order_by("distance")
+                .first()
+            )
+        else:
+            return None
